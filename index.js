499962b90292eb726d1ac21661d47fe3ff38ec0d
Trang Hoang <tychuotsieu45@gmail.com>
2020-04-30 10:51:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   // .set('views', path.join(__dirname, 'views'))
   // .set('view engine', 'ejs')
+  .get('/home', (req, res) =>
+                res.sendFile(path.join(__dirname, 'public/meal.html')))
   .get('/meal', (req, res) =>
                 res.sendFile(path.join(__dirname, 'public/meal.html')))
   .get('/drink', (req, res) =>
